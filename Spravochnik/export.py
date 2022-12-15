c964d1d73e8ebfeeda664cc037366f676d132059
Alex <juzzle@mail.ru>
2022-12-15 14:19:40 +0300

    update export

--- Spravochnik/export.py
+++ Spravochnik/export.py
@@ -1,27 +1,27 @@
 import csv
+list_find_values = []
 
 
 def exp(str_find):
-
+   
     with open('db.csv', encoding='utf-8') as f:
-        reader = csv.reader(f)
+        list_find_values = csv.reader(f)
         result_list = []
+        for val in list_find_values:
+            if find_val(str_find, val):
+                result_list.append(val)
+        some_list = []
+        for i in result_list:
+            if i not in some_list:
+                some_list.append(i)
+        return some_list
 
-        for i in reader:
-            count = 0
-            if str_find[count] in i:
-                count += 1
-                if len(str_find) > 2:
-                    for a in i:
-                        if str_find[count] in a:
-                            count += 1
-                            if len(str_find) > 2:
-                                for e in i:
-                                    if str_find[count] in e:
-                                        result_list.append(i)
-                                    else: count = 2
-                            else: result_list.append(i)
-                        else:count = 1
-                else : result_list.append(i)
-            else: count = 0
-        return result_list
+def find_val(str_val, list_val):
+    count = 0
+    string_lenght= len(str_val)
+    for i in list_val:
+        if i in str_val:
+            count +=1
+    if count == string_lenght:
+        return True
+    else : False
